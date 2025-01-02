47f78d711a2c1fcac0ee3e7788df840e5c1a0fc2
Tanishq Harit <pheonix4563@gmail.com>
2025-01-02 15:53:21 -0500

    2nd_Jan_2025

---
+++ N2_Python_Programming/Getting_Started.py
@@ -0,0 +1,31 @@
+#------------------------------------------------------------------------------------------------------------
+# Print Function (printf), Variables & Basic Data Types 
+
+print("Neetcode.io")
+
+# Variables 
+name = "Tanishq"
+age = 24
+graduate_university = "Stevens Institute of Technology"
+my_current_gpa = 3.6
+expected_graduation = None
+my_message = "Getting Started with Python!"
+undergraduate_university, undergraduate_gpa = "Delhi College of Engineering", 7.0
+
+# Print Statements
+print(f"Hello, my name is {name} and I am {age} years old. I am {my_message}")
+print(f"Currently studying in {graduate_university} and having a gpa of {my_current_gpa}.")
+print(f"My expected graduate date is {expected_graduation}.")
+print(f"I have completed my undergraduate from {undergraduate_university} with a gpa of {undergraduate_gpa}")
+
+# Output
+"""             Output of the code (Terminal)
+Neetcode.io
+Hello, my name is Tanishq and I am 24 years old. I am Getting Started with Python!
+Currently studying in Stevens Institute of Technology and having a gpa of 3.6.
+My expected graduate date is None.
+I have completed my undergraduate from Delhi College of Engineering with a gpa of 7.0
+"""
+
+#------------------------------------------------------------------------------------------------------------
+# 
